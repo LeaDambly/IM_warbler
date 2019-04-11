702e79ddaba179f61305774f3c1172f11bfb5d9f
LeaDambly <leadam@ceh.ac.uk>
2019-04-11 16:34:28 +0100

    Added PA BBA Getting BBS data

--- 01_IM_warbler.R
+++ 01_IM_warbler.R
@@ -4,3 +4,26 @@
 # Data: BBS, eBird, PA bird atlas
 # Cov: Elevation and forest cover
 
+# libraries----
+library(rBBS)
+
+# data prep----
+# PA BBA from Miller et al. appendix
+# BBS
+RegionMetaData <- GetRegions()
+WeatherMetaData <- GetWeather()
+# Bob FYI, your GetRoutes function has an issue. It calls Routes.zip instead of routes.zip 
+GetRoutes <- function(Dir="ftp://ftpext.usgs.gov/pub/er/md/laurel/BBS/DataFiles/") {
+  routes=GetUnzip(ZipName=paste0(Dir, "routes.zip"), FileName="routes.csv")
+  routes$routeID=paste(routes$statenum, routes$Route)
+  routes
+}
+RoutesMetaData <- GetRoutes()
+
+PACode <- RegionMetaData$RegionCode[RegionMetaData$`State/Prov/TerrName` == "PENNSYLVANIA"]
+PAYears <- 2005:2009
+
+PAWren<- GetRouteData(AOU=6540, countrynum = 840, states = PACode, year = PAYears,
+                      weather = WeatherMetaData, routes = RoutesMetaData,
+                      Zeroes = TRUE)
+# leads to warnings();  In FUN(X[[i]], ...) : no ID, so setting to NA
